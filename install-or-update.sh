d3d82e6d3008a3df32b48ab18de66d4900e2f165
Matheus da Silva Kruger <ms-kruger@hotmail.com>
2023-01-18 17:15:04 -0300

    Fixing install script

--- install-or-update.sh
+++ install-or-update.sh
@@ -6,9 +6,9 @@ echo "--- Building executable ---"
 cargo build -r
 
 echo "--- Copying executable to your HOME folder ---"
-CP target/release/tinywm-rust $HOME/.tinywm-rust/tinywm-rust
+CP target/release/tinywm-rust /usr/bin/tinywm-rust/tinywm-rust
 
 echo "--- Copying desktop entry to your sessions folder ---"
-sudo cp tinywm.desktop /usr/share/xsessions/tinywm.desktop
+cp tinywm.desktop /usr/share/xsessions/tinywm.desktop
 
 echo "Installation/Update process finishid. Log out and Log in again using TinyWM!"
